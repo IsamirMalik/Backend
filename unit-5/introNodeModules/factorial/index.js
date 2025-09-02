b8aed016e3784671dd7d97adba3761c0453b4566
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2025-09-02 13:14:42 +0000

    add factorial module

---
+++ unit-5/introNodeModules/factorial/index.js
@@ -0,0 +1,9 @@
+const factorial = require("./factorial");
+
+const final = factorial(5);
+const final1 = factorial(10);
+const final2 = factorial(6);
+
+console.log(final)
+console.log(final1)
+console.log(final2)
